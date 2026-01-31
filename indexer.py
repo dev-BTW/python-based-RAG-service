48237c5bad698bc2bc0296a93d091e765070b8bc
Parth More <144650668+dev-BTW@users.noreply.github.com>
2026-01-31 22:15:35 +0530

    replaced depricated imports

--- indexer.py
+++ indexer.py
@@ -1,7 +1,7 @@
 import json
 import faiss
 import numpy as np
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
 
 class Indexer:
